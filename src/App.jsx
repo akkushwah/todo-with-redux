f754f1b4d6810a0c2595c8a1a9886dd277d3fd9d
akanksha <akankshakushwah1501@gmail.com>
2024-08-09 19:01:33 +0530

    Fix Error

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import Todo from "./Todo"
 import Home from "./Home"
 
 function App() {
@@ -6,7 +5,6 @@ function App() {
   return (
     <>
       <Home />
-      {/* <Todo /> */}
     </>
   )
 }
